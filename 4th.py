a02c63f66faab2ac51e5130448118e946a32a488
Leonardoo31 <68443532+Leonardoo31@users.noreply.github.com>
2020-07-21 20:46:08 +0300

    Add files via upload

---
+++ 4th.py
@@ -0,0 +1,12 @@
+
+#fizz buzz
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "Fizzbuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "buzz"
+    return input
+
+print(fizz_buzz(int(input("say a number: "))))
